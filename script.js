3a96adaf5ba13054df60acf286eeec175006d2b9
Luciana Murata <midori9213@gmail.com>
2020-07-27 22:00:54 -0300

    definir movimento da cobrinha

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * box;
-    y: 8* box;
+    x: 8 * box, 
+    y: 8* box
 }
 let direction = "right";
 
@@ -33,6 +33,16 @@ function iniciarJogo(){
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
-}
 
-let jogo = setInterval(iniciarJogo, 100);
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+ 
+let jogo = setInterval(iniciarJogo, 100);  // vai reinicializar a cada 100ms (intervalo), sem travar
+
