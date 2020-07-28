bf9acc164893ac8d1ff25309b4cef1e78ba44f8f
Luciana Murata <midori9213@gmail.com>
2020-07-27 22:59:28 -0300

    alimentando a cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ snake[0] = {
     y: 8* box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+
+    //math.random traz um número aleatório até 1
+    //math.floor tira a parte flutuante do math random (não deixa numero quebrado)
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,6 +26,11 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update); 
@@ -39,6 +51,7 @@ function iniciarJogo(){
     
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
